e1ae66a3bb6f0a519664b7f68763eb2a324e4e66
Zak <zakadmaster@gmail.com>
2020-02-14 09:48:05 +0100

    Addition of cards from api

---
+++ client/src/Components/PlayerCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+export default class PlayerCard extends Component {
+
+    render() {
+        const { name, country } = this.props
+        return(
+            <div>
+                {name}, {country}
+            </div>
+        )
+    }
+}
---
+++ client/src/Components/PlayerList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PlayerCard from './PlayerCard';
+
+export default class PlayerList extends Component {
+    
+    render() {
+        const { players } = this.props;
+
+        return(
+            <div>
+                { players.map(player => {
+                    console.log(player)
+                    return(
+                        <PlayerCard key={player.id} name={player.name} country={player.country}/>
+                    )
+                }) }
+            </div>
+        )
+    }
+        
+    
+}
